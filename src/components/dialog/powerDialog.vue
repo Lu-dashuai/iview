1b9d8ffab1bf5a12d88434decd5e7f0ac74e2130
Lu-dashuai <aluruihua@163.com>
2019-10-19 19:27:25 +0800

    完成授权

--- src/components/dialog/powerDialog.vue
+++ src/components/dialog/powerDialog.vue
@@ -18,7 +18,7 @@ export default {
       data2: [
       ],
       _if:[],//向后台发送的权限菜单
-      row:{}
+      row:{},
     };
   },
   created:function(){
@@ -31,24 +31,58 @@ export default {
       this.getPower();
     },
     getPower(){
+      this.data2 = [];
       // alert(this.row.id);
+        //获取所有菜单用于展示
       this.$http
         // .get("power/getPower",{ params: { role_id: this.row.id } }) //通过role_id获取权限菜单树
         .get("power/getPower",{ params: { role_id: null } }) //
         .then(res => {
           // alert(JSON.stringify(res))
           if (res.status == '200') {
-            this.data2 = res.body
-            for(var i = 0;i<this.data2.length;i++){
-              this.data2[i].expand = true
-            }
+            
+            //获取该角色已经存在的菜单
+            var temp = [];
+            this.$http
+              .get("power/getPower",{ params: { role_id: this.row.id } }) //通过role_id获取权限菜单树
+              // .get("power/getPower",{ params: { role_id: null } }) //
+              .then(res2 => {
+                if (res2.status == '200') {
+                  var parent = res2.body;
+                  for(var i=0;i<parent.length;i++){
+                    temp.push(parent[i].id);
+                    var child = parent[i].children;
+                    for(var j =0;j<child.length;j++){
+                        temp.push(child[j].id);
+                    }
+                  }
+                  // alert(temp)
+                  //给所有的父节点添加expand=ture属性，让节点打开
+                  var father = res.body;
+                  // alert(JSON.stringify(father))
+                  for(var k=0;k<father.length;k++){
+                    father[k].expand = true;
+                    var chi = father[k].children;
+                    for(var z=0;z<chi.length;z++){
+                      if(temp.indexOf(chi[z].id)>-1){
+                        chi[z].checked = true;
+                      }
+                    }
+                  }
+                  this.data2 = res.body;//给dialog赋值
+                } else {
+                  this.$Message.error("获取数据失败!"); 
+                }
+              })//第二次请求结束
+            
           } else {
             this.$Message.error("获取数据失败!"); 
           }
         })
+
+        
     },
     choiceAll:function(data){
-// 　　　　alert(JSON.stringify(data));  //当复选框选中时则会打印出选中的内容
         this._if = data
 　　},
     handleSubmit(){
